c3ffd97237182e17fd7930eb7ea7a3d7da5bf19b
lodnsec-cem7 <dutanqian95971@126.com>
2021-06-01 11:17:54 +0800

    配置数据库

--- manage.py
+++ manage.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
-
+#  加载配置
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return 'yi shen'
+    return 'index'
 
 
 if __name__ == '__main__':
