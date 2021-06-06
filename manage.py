09170cb671e08ab2478aa04a41a133842a8ccdad
lodnsec-cem7 <dutanqian95971@126.com>
2021-06-06 18:44:26 +0800

    update

--- manage.py
+++ manage.py
@@ -1,22 +1,11 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
-class Config(object):
-    """项目的配置"""
-    DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
-#  加载配置
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
-    return 'index'
+    return 'index222'
 
 
 if __name__ == '__main__':
